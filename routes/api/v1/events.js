35bf47e34c4915c02559112a925e35fbc8f5bf6e
Tom Quinn <Tquinn2135@gmail.com>
2025-12-08 13:57:53 -0600

    Changed  event endpoint to accept ObjectId

--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const eventRouter = require('express').Router()
-const { getCollection } = require('../../../dbconnect')
+const { getCollection, ObjectId } = require('../../../dbconnect')
 
 //get all events
 eventRouter.get('/', async (req, res) => {
@@ -11,20 +11,21 @@ eventRouter.get('/', async (req, res) => {
 
 //add a new event
 eventRouter.post('/add', async (req, res) => {
-    const { name, location, date, time, eventID } = req.body
+    const { name, location, date, time } = req.body
     const collection = await getCollection('FoodTruck', 'Events')
-    const { acknowledged } = await collection.insertOne({ name, location, date, time, eventID })
+    const { acknowledged } = await collection.insertOne({ name, location, date, time })
     res.send({ acknowledged })
 })
 
 
+
 //get event by eventID
-eventRouter.get('/:eventID', async (req, res) => {
-    const { eventID } = req.params    
+eventRouter.get('/:id', async (req, res) => {
+    const { id } = req.params
     const collection = await getCollection('FoodTruck', 'Events')
-    const found = await collection.findOne({ eventID: parseInt(eventID) })
-    if(found) res.json(found)
-     else res.send({error: `Event not found: ${eventID}`})
+    const found = await collection.findOne({ _id: new ObjectId(id) })
+    if (found) res.send(found)
+        else res.send({error: {message: `event with id: ${id} not found`}})
 })
 
 
