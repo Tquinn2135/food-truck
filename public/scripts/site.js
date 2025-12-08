f7b6a39ab7cb38189c08b503336775a6a4093982
allenreed445 <allenree255@gmail.com>
2025-12-08 12:53:39 -0600

    first commit

--- public/scripts/site.js
+++ public/scripts/site.js
@@ -1,13 +1,41 @@
-(async() =>{
-     const truckLocation = document.getElementById('truckLocation')
-     const menuItems = document.getElementById('menuItems')
+(async () => {
+     const truckLocation = document.querySelector('truck-location')
+     const menuItems = document.querySelector('.menu-items')
 
-     const { pathname } = window.location
-     const [, searchType, id ] = pathname.split('/')
+     const { pathname} = window.location
+     const [, searchType, id] = pathname.split('/')
 
-     //continue here
+     if (searchType === 'trucks' && id) {
+          const menuRes = await fetch(`/api/v1/menu`)
+          const menuData = await menuRes.json()
+     }
 
+     menuItems.innerHTML = menuData
+     .map(item => `
+     <div class="menu-item">
+     <h4>${item.name}</h4>
+     <img src="${item.imageUrl}" width="150 />
+     <p>${item.description}</p>
+     <strong>$${item.price}</strong>
+     </div>
+     `)
+
+     .join('')
+
+     const eventRed = await fetch('api/v1/events')
+     const eventData = await eventRed.json()
+     truckLocation.innerHTML = eventData
      
-})()
+     .map(event => `
+     <div class="event-item">
+     <h4>
+     <a href="/event/${event.id}">
+     ${event.name}
+     </a>
+     </h4>
+     <p>${event.date}</p>
+     </div>
+     `)
+     .join('')
 
-
+})
