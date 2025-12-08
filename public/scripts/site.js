63e016f332ab5201809d0a9753ae8304cd55ff78
allenreed445 <allenree255@gmail.com>
2025-12-08 13:01:29 -0600

    continued work on site.js

--- public/scripts/site.js
+++ public/scripts/site.js
@@ -1,54 +1,53 @@
 (async () => {
-     const truckLocation = document.querySelector('truck-location')
-     const menuItems = document.querySelector('.menu-items')
+  const truckLocation = document.getElementById('truckLocation')
+  const menuItems = document.getElementById('menuItems')
 
-     const { pathname} = window.location
-     const [, searchType, id] = pathname.split('/')
+  const { pathname } = window.location
+  const [, firstSegment, id] = pathname.split('/') // "" | "event" | ...
 
-     if (searchType === 'trucks' && id) {
-          const menuRes = await fetch(`/api/v1/menu`)
-          const menuData = await menuRes.json()
-     }
+  if (pathname === '/' || pathname === '') {
 
-     menuItems.innerHTML = menuData
-     .map(item => `
-     <div class="menu-item">
-     <h4>${item.name}</h4>
-     <img src="${item.imageUrl}" width="150 />
-     <p>${item.description}</p>
-     <strong>$${item.price}</strong>
-     </div>
-     `)
+    const menuRes = await fetch('/api/v1/menu')
+    const menuData = await menuRes.json()
 
-     .join('')
+    menuItems.innerHTML = menuData
+      .map(item => `
+        <div class="menu-item">
+          <h4>${item.name}</h4>
+          <img src="${item.imageUrl}" width="150" />
+          <p>${item.description}</p>
+          <strong>$${item.price}</strong>
+        </div>
+      `)
+      .join('')
 
-     const eventRed = await fetch('api/v1/events')
-     const eventData = await eventRed.json()
-     truckLocation.innerHTML = eventData
+    
+    const eventRes = await fetch('/api/v1/events')
+    const eventData = await eventRes.json()
 
-     .map(event => `
-     <div class="event-item">
-     <h4>
-     <a href="/event/${event.id}">
-     ${event.name}
-     </a>
-     </h4>
-     <p>${event.date}</p>
-     </div>
-     `)
-     .join('')
-     
-     if (searchType === 'events' && id) {
-          const res = await fetch(`/api/v1/events/${id}`)
-          const event = await res.json()
+    truckLocation.innerHTML = eventData
+      .map(event => `
+        <div class="event-item">
+          <h4>
+            <a href="/event/${event._id}">
+              ${event.name}
+            </a>
+          </h4>
+          <p>${event.date}</p>
+        </div>
+      `)
+      .join('')
+  }
 
-          truckLocation.innerHTMN =`
-          <h2>${event.name}</h2>
-          <p><strong>Location:</strong> ${event.location}</p>
-          <p><strong>Date:</strong> ${event.date}</p>
-          <p><strong>Time:</strong> ${event.time}</p>
-     
-          `
+  if (firstSegment === 'event' && id) {
+    const res = await fetch(`/api/v1/events/${id}`)
+    const event = await res.json()
 
-     }
+    truckLocation.innerHTML = `
+      <h2>${event.name}</h2>
+      <p><strong>Location:</strong> ${event.location}</p>
+      <p><strong>Date:</strong> ${event.date}</p>
+      <p><strong>Time:</strong> ${event.time}</p>
+    `
+  }
 })()
